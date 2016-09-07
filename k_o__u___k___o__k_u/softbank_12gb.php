c8202ac78c19b16b56b042d9fec6e5443ac65cf1
PanNyaa <pan.nyaa.ccc@gmail.com>
2016-09-08 00:26:05 +0900

    広告を追加

--- k_o__u___k___o__k_u/softbank_12gb.php
+++ k_o__u___k___o__k_u/softbank_12gb.php
@@ -1,5 +1,7 @@
-setTimeout("link()", 0);
-function link(){
-location.href='https://px.a8.net/svt/ejp?a8mat=2NST33+DT4YWI+3KKM+5Z6WX';
-}
 
+<?php
+
+header('location: https://px.a8.net/svt/ejp?a8mat=2NST33+DT4YWI+3KKM+5Z6WX');
+exit();
+
+?>
