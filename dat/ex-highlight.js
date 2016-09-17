50ebbf80285f5f441474ae5b53d0cd241090b561
PanNyaa <pan.nyaa.ccc@gmail.com>
2016-09-17 17:38:14 +0900

    ハイライターの言語名ヘッダ表示のやつを作りちゅう()ローカルでテストすればいいのにね

--- dat/ex-highlight.js
+++ dat/ex-highlight.js
@@ -3,9 +3,12 @@ jQuery(function(){
     jQuery("pre code").each(function(i, block) {
         console.log(block.classList[1]);
         
-        
+        var pre = document.getElementsByTagName('pre');	//よくわからない
+		console.log(pre);
+
         
     });
    
 
 });
+
