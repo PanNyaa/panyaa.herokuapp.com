09505bf4b81a4d6e1f4f07c856323d6b3f4049b7
PanNyaa <pan.nyaa.ccc@gmail.com>
2016-09-18 00:09:39 +0900

    言語名をちゃんと表示するようにした

--- dat/ex-highlight.js
+++ dat/ex-highlight.js
@@ -1,32 +1,43 @@
+//言語名を大文字化したりなんかしたりするやつ。目についたら追加していく
+function LangNameNormalize(str){
+    if(str == "php")return "PHP";
+    if(str == "cs")return "C#";
+    if(str == "javascript")return "JavaScript";
+    if(str == "perl")return "Perl";
+    if(str == "bash")return "Bash";
+}
+
 //WordPressで使うときは $ じゃなくて jQuery にしないとダメみたい……
 //無名関数なので即時実行されるぞ！
 jQuery(function(){
-	
-	//pre には配列で<pre>にアクセス可能なアレがpreの個数分入る
-	var pre = document.getElementsByTagName('pre');
 
-	//console.log(pre);の実行結果 HTMLCollection [ <pre>, <pre>, <pre>, <pre> ]
-	//たとえばpreが4個見つかったのならpre[0] ~ pre[3] でいじれるぞ！
+    //pre には配列でpreにアクセス可能なアレがpreの個数分入る
+    var pre = document.getElementsByTagName('pre');
 
-	//eachでpre codeの数だけループされるぞ！
+    //たとえばpreが4個見つかったのならpre[0] ~ pre[3] でいじれるぞ！
+
+    //eachでpre codeの数だけループされるぞ！
     jQuery("pre code").each(function(i, block) {
+        
+        var j,div;
+        
         //block.classList[1]にはcodeタグのクラスの二番目(hljs php←これ)が入ってるぞ！
         //codeに予めクラス名で言語指定していた場合はこの順番が逆転するのでそれへの対応
-        var j = block.classList[1] != 'hljs' ? 1 : 0;
+        j = block.classList[1] != 'hljs' ? 1 : 0;
         
-        //divタグで作りまーす
-        var div = document.createElement('div');
+        //divタグで作りまーす。div変数はオブジェクトになるのかな？
+        div = document.createElement('div');
         
         //言語名追加
-        div.textContent = block.classList[j];
-
-		//divにclass追加、CSSでいじれ！
-		div.classList.add('hl-header');
-		
-		//pre要素の最初の子要素として追加
-		pre[i].insertBefore(div,pre[i].firstChild);
         
+        div.textContent = LangNameNormalize(block.classList[j]);
+
+        //divにclass追加、CSSでいじれ！
+        div.classList.add('hl-header');
+
+        //pre要素の最初の子要素として追加
+        pre[i].insertBefore(div,pre[i].firstChild);
+
     });
-   
 
 });
