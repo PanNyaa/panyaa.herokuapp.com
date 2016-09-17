8e1ff2837701cfc0b4ac6e13c8b811eed4459dad
PanNyaa <pan.nyaa.ccc@gmail.com>
2016-09-17 17:51:09 +0900

    ハイライターの言語名ヘッダ表示のやつを作りちゅう()ローカルでテストすればいいのにね

--- dat/ex-highlight.js
+++ dat/ex-highlight.js
@@ -1,10 +1,25 @@
 jQuery(function(){
+	
+	var pre[] = document.getElementsByTagName('pre');	//pre には配列で<pre>にアクセス可能なアレがpreの個数分入る
+	
+	//console.log(pre);
+	//↑の実行結果 HTMLCollection [ <pre>, <pre>, <pre>, <pre> ]
+	//たとえばpreが4個見つかったのならpre[0] ~ pre[3] でいじれるぞ！
 
     jQuery("pre code").each(function(i, block) {
-        console.log(block.classList[1]);
+        console.log(block.classList[1]);	//block.classList[1]でpre codeタグのクラスの二番目(hljs php←これ)が入ってるぞ！
         
-        var pre = document.getElementsByTagName('pre');	//よくわからない
-		console.log(pre);
+        var div = document.createElement('div');
+        div.textContent = block.classList[1];
+
+		//class追加
+		element.classList.add('hl-header');
+		
+		
+		
+		
+		
+		pre[i].appendChild(div);
 
         
     });
