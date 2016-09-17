6e31b8e7a7452dbfad416fe863cf2f956c856e41
PanNyaa <pan.nyaa.ccc@gmail.com>
2016-09-17 11:18:06 +0900

    ハイライターの言語名ヘッダ表示をhighlight.jsの中にくみこんだ

--- wp-content/themes/plain-blog/footer.php
+++ wp-content/themes/plain-blog/footer.php
@@ -45,7 +45,7 @@
     <?php wp_footer(); ?>
     <script src="/dat/highlight.pack.js"></script>
     <script>hljs.initHighlightingOnLoad();</script>
-    <script src="/dat/exlang-highlight.js"></script>
+    <script>highlight_head();</script>
     <script src="/dat/jammer.js"></script>
 </body>
 </html>
