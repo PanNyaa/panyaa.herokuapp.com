5042bc09bb2a0816baf3e4092053cf5853289a78
PanNyaa <pan.nyaa.ccc@gmail.com>
2016-09-19 23:42:56 +0900

    はいはいvar使わなきゃいいんでしょ無理やり関数にしてやるんだから！！

--- dat/ex-highlight.js
+++ dat/ex-highlight.js
@@ -8,40 +8,35 @@ function LangNameNormalize(str){
     return str;
 }
 
-//WordPressで使うときは $ じゃなくて jQuery にしないとダメみたい……
-//無名関数なので即時実行されるぞ！
-jQuery(function(){
-
-    //pre には配列でpreにアクセス可能なアレがpreの個数分入る
-    var pre = document.getElementsByTagName('pre');
-
-    //たとえばpreが4個見つかったのならpre[0] ~ pre[3] でいじれるぞ！
-
-    //eachでpre codeの数だけループされるぞ！
-    jQuery("pre code").each(function(i, block) {
-        
-        var j,div;
+//エレメント追加するルーチン
+function AddElements(elm){
+   
+   jQuery("pre code").each(function(i, block){
         
         //block.classList[1]にはcodeタグのクラスの二番目(hljs php←これ)が入ってるぞ！
-        //codeに予めクラス名で言語指定していた場合はこの順番が逆転するのでそれへの対応
-        j = block.classList[1] != 'hljs' ? 1 : 0;
-        
         //divタグで作りまーす。div変数はオブジェクトになるのかな？
-        div = document.createElement('div');
+        const div = document.createElement('div');
         
         //言語名追加
-        div.textContent = LangNameNormalize(block.classList[j]);
+        //codeに予めクラス名で言語指定していた場合はこの順番が逆転するのでそれへの対応も
+        div.textContent = LangNameNormalize(block.classList[block.classList[1] != 'hljs' ? 1:0 ]);
 
         //divにclass追加、CSSでいじれ！
         div.classList.add('hl-header');
 
         //pre要素の最初の子要素として追加
-        pre[i].insertBefore(div,pre[i].firstChild);
-        
-        //ここまではdiv hl-header要素の追加
-        
-        
+        elm[i].insertBefore(div,pre[i].firstChild);
 
     });
+   
+}
+
+//WordPressで使うときは $ じゃなくて jQuery にしないとダメみたい……
+//無名関数なので即時実行されるぞ！
+jQuery(function(){
+
+    //pre には配列でpreにアクセス可能なアレがpreの個数分入る
+    //たとえばpreが4個見つかったのならpre[0] ~ pre[3] でいじれるぞ！
+    AddElements(document.getElementsByTagName('pre'));
 
 });
