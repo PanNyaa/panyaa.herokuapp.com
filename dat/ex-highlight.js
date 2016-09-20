29163cce4e28853a600022252caff99334172eb2
PanNyaa <pan.nyaa.ccc@gmail.com>
2016-09-21 00:37:13 +0900

    お勉強のためにex-highlight.jsでjqueryを使わないように作り直してみた

--- dat/ex-highlight.js
+++ dat/ex-highlight.js
@@ -1,35 +1,31 @@
-//言語名を大文字化したりなんかしたりするやつ。目についたら追加していく
-function LangNameNormalize(str){
-    if(str == "php")return "PHP";
-    if(str == "cs")return "C#";
-    if(str == "javascript")return "JavaScript";
-    if(str == "perl")return "Perl";
-    if(str == "bash")return "Bash";
-    return str;
+//言語名を大文字化したりするやつ。目についたら追加していく
+function LangNameNormalize(str) {
+  if (str == 'php') return 'PHP';
+  if (str == 'cs') return 'C#';
+  if (str == 'javascript') return 'JavaScript';
+  if (str == 'perl') return 'Perl';
+  if (str == 'bash') return 'Bash';
+  return str;
 }
 
-//WordPressで使うときは $ じゃなくて jQuery にしないとダメみたい……だけどjqueryを使わないようにしてみたぞ！
 //無名関数なので即時実行されるぞ！
-!(function(){
-
-    //blockには配列でpreにアクセス可能なアレがpreの個数分入る
-    //たとえばpreが4個見つかったのならblock[0] ~ block[3] でいじれるぞ！
-    document.querySelectorAll("pre code").forEach(function(block,i){
-        
-        //追加するためのdiv要素の定義
-        const div = document.createElement('div');
-        
-        //言語名追加
-        //block.classList[1]にはcodeタグのクラスの二番目(hljs php←これ)が入ってるぞ！
-        //codeに予めクラス名で言語指定していた場合はこの順番が逆転するのでそれへの対応も
-        div.textContent = LangNameNormalize(block.classList[ block.classList[1] != 'hljs' ? 1:0 ]);
-        
-        //divにclass追加、CSSでいじれ！
-        div.classList.add('hl-header');
-        
-        //pre要素の最初の子要素として追加
-        block[i].insertBefore(div,elm[i].firstChild);
-        
-    });
-
+!(function () {
+  //document.querySelectorAllで返ってくるのはオブジェクトらしいので配列に変換するやつ
+  Array.prototype.forEach.call(
+    //pre code要素を全部取得
+    document.querySelectorAll('pre code'),function (block,i){
+      
+      //追加する用のdiv要素を定義
+      const div = document.createElement('div');
+      
+      //言語名追加、hljsクラスと言語名クラスの順番が逆転していることがあるのでそれへの対応も
+      div.textContent = LangNameNormalize(block.classList[block.classList[1] != 'hljs' ? 1 : 0]);
+      
+      //追加するdivにクラスを追加、CSSでいじれるぞ！
+      div.classList.add('hl-header');
+      
+      //要素をHTMLに追加
+      block[i].insertBefore(div, block[i].firstChild);
+    }
+  );
 });
