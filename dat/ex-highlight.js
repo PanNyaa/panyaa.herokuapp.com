7404c1c785651ab0467834ad75d02a44c94af467
PanNyaa <pan.nyaa.ccc@gmail.com>
2016-09-20 23:57:50 +0900

    お勉強のためにex-highlight.jsでjqueryを使わないように作り直してみた

--- dat/ex-highlight.js
+++ dat/ex-highlight.js
@@ -8,35 +8,27 @@ function LangNameNormalize(str){
     return str;
 }
 
-//エレメント追加するルーチン
-function AddElements(elm,div){
-   
-   jQuery("pre code").each(function(i, block){
+//WordPressで使うときは $ じゃなくて jQuery にしないとダメみたい……
+//無名関数なので即時実行されるぞ！
+!(function(){
+
+    //blockには配列でpreにアクセス可能なアレがpreの個数分入る
+    //たとえばpreが4個見つかったのならblock[0] ~ block[3] でいじれるぞ！
+    document.querySelectorAll("pre code").forEach(function(block,i){
         
-        //divタグで作りまーす。div変数はオブジェクトになるのかな？
         const div = document.createElement('div');
         
         //言語名追加
         //block.classList[1]にはcodeタグのクラスの二番目(hljs php←これ)が入ってるぞ！
         //codeに予めクラス名で言語指定していた場合はこの順番が逆転するのでそれへの対応も
         div.textContent = LangNameNormalize(block.classList[ block.classList[1] != 'hljs' ? 1:0 ]);
-
+        
         //divにclass追加、CSSでいじれ！
         div.classList.add('hl-header');
-
+        
         //pre要素の最初の子要素として追加
-        elm[i].insertBefore(div,elm[i].firstChild);
-
+        block[i].insertBefore(div,elm[i].firstChild);
+        
     });
-   
-}
-
-//WordPressで使うときは $ じゃなくて jQuery にしないとダメみたい……
-//無名関数なので即時実行されるぞ！
-jQuery(function(){
-
-    //pre には配列でpreにアクセス可能なアレがpreの個数分入る
-    //たとえばpreが4個見つかったのならpre[0] ~ pre[3] でいじれるぞ！
-    AddElements(document.getElementsByTagName('pre'));
 
 });
