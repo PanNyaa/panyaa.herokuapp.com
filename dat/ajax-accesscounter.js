2ed7306562ab62fdf7e9c8f80ccd400ac19b2cbf
PanNyaa <pan.nyaa.ccc@gmail.com>
2016-09-25 18:26:19 +0900

    ajaxでJavascriptからPHPに値を受け渡してPHPを実行してみるテスト

--- dat/ajax-accesscounter.js
+++ dat/ajax-accesscounter.js
@@ -1,12 +1,13 @@
 !function(){
 var req = new XMLHttpRequest();
 req.onreadystatechange = function() {
+  var result = document.this;
   if (req.readyState == 4) { // 通信の完了時
     if (req.status == 200) { // 通信の成功時
-      this.innerHTML = req.responseText;
+      result.innerHTML = req.responseText;
     }
   }else{
-    this.innerHTML = "まだだよ"
+    result.innerHTML = "まだだよ"
   }
 }
 
