cc09698f9616786dd5d80c733efd1ef9af2a2eca
PanNyaa <pan.nyaa.ccc@gmail.com>
2016-09-26 00:44:48 +0900

    ajaxでアクセスカウンターするやつテスト

--- dat/accesscounter.php
+++ dat/accesscounter.php
@@ -4,7 +4,7 @@
     ini_set( 'display_errors', 1 );                     //エラーメッセージを表示する設定にする
     //手動で配置したdropbox-sdkを読み込む。ファイルパスはphpの場所に応じて適宜書き換えてください。
     //require_once dirname(__FILE__)."/../../../lib/Dropbox/autoload.php";
-    require('./vendor/autoload.php'); //heroku上にdropbox-sdkがインストールできたのでどこからでもお手軽インクルード
+    require('../vendor/autoload.php'); //heroku上にdropbox-sdkがインストールできたのでどこからでもお手軽インクルード
     // \Dropbox を dbx としても記述できるように定義する
     use \Dropbox as dbx;
     //自作関数を入れるので新しいクラスを dbx\Client から継承して作る、継承むずかしいです！！
