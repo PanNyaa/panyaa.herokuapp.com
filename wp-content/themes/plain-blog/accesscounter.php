2efc49eee0905c7af7122ccd223534a225318524
PanNyaa <pan.nyaa.ccc@gmail.com>
2016-09-26 15:37:24 +0900

    FingerHashで二重アクセスカウントの判別

--- wp-content/themes/plain-blog/accesscounter.php
+++ wp-content/themes/plain-blog/accesscounter.php
@@ -41,7 +41,7 @@
     //手動で配置したdropbox-sdkを読み込む。ファイルパスはphpの場所に応じて適宜書き換えてください。
     //require_once dirname(__FILE__)."/../../../lib/Dropbox/autoload.php";
     //require_once('/app/vendor/autoload.php'); //heroku上にdropbox-sdkがインストールできたのでどこからでもお手軽インクルード
-    require($_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php');
+    require($_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php');  //Documentrootからvendorを読み込む…
     //namespace Controllers;
 
     // \Dropbox を dbx としても記述できるように定義する
@@ -91,20 +91,22 @@
     $counts = [0];
 
     $ip = $_SERVER['REMOTE_ADDR'];  //アクセス元のIPアドレスを取得
-    $ipdata = "0.0.0.0 0.0.0.0";;   //仮データを入れておく
+    $ipdata = "0.0.0.0 0.0.0.0";   //仮データを入れておく
     $iptable = [0];
     
-    //$finger_print = new Fingerprint2().get(function(result){console.log(result);});
+    $fingerhash = $_REQUEST['fpk']; //ajaxで渡されたFingerPrintのハッシュデータを取得
+    $fingerhashdata = "0000000000000000 0000000000000000";  //仮データを入れておく
+    $fingerhashtable = [0];
 
-    $path_countdata = "/data/countdata.dat";    //ファイルパスなので適宜いじってください
-    $path_ipdata = "/data/ipdata.dat";          //※ファイルパスの先頭には / もしくは ./ が必須なようです(無い場合はsdk側でエラーが出る)
-                                                //例えば /data/a.dat と指定した場合、アプリケーション指定したフォルダの直下にdata/a.datが生成されます
-                                                //例えば ./data/a.dat と指定した場合、このphpを実行しているフォルダの場所の構成がそのままアプリケーション指定したフォルダの直下に生成されます
-                                                //例えばheroku上で /wp-content/themes/plain-blog/accsesscounter.php で実行している場合、./data/a.dat と指定すると、
-                                                //      (アプリケーションフォルダ名)/app/wp-content/themes/plain-blog/data/a.dat が生成されます(/app/が間に挟まるのはherokuの特性です)
+    $path_countdata = "/data/countdata.txt";            //ファイルパスなので適宜いじってください
+    $path_ipdata = "/data/ipdata.txt";                  //※ファイルパスの先頭には / もしくは ./ が必須なようです(無い場合はsdk側でエラーが出る)
+    $path_fingerhashdata = "/data/fingerhashdata.txt";   //例えば /data/a.dat と指定した場合、アプリケーション指定したフォルダの直下にdata/a.datが生成されます
+                                                        //例えば ./data/a.dat と指定した場合、このphpを実行しているフォルダの場所の構成がそのままアプリケーション指定したフォルダの直下に生成されます
+                                                        //例えばheroku上で /wp-content/themes/plain-blog/accsesscounter.php で実行している場合、./data/a.dat と指定すると、
+                                                        //  (アプリケーションフォルダ名)/app/wp-content/themes/plain-blog/data/a.dat が生成されます(/app/が間に挟まるのはherokuの特性です)
+                                                        //絶対パスを指定すればそのままのパスでdropboxに作成されます
     $ipflag = true;
-    $session_flag = true;
-
+    $repeatflag = true;
 
     $countdata = $client->getFileContents($path_countdata); //dropboxからファイル(これはアクセス数カウントデータ)を読み込んで文字列に返す
     $counts = split(" ", $countdata);   //トータルアクセス数・今日のアクセス数・昨日のアクセス数・日付データを半角スペースで分解して格納
@@ -113,40 +115,58 @@
         $counts[3] = date("j");         //日付$count[3]を今日の日付に
         $counts[2] = $counts[1];        //昨日のカウント$count[2]を$count[1]に
         $counts[1] = 0;                 //今日のカウント$count[1]を0に
-        $iptable = split(" ", $ipdata); //入っている仮データを分割して配列にぽいぽい
+        $fingerhashtable = split(" ", $fingerhashdata); //仮データを半角スペースで分解して配列に格納(FingerHash)、昨日の分を上書き
     }else{
-        $ipdata = $client->getFileContents($path_ipdata);   //dropboxからファイル(これはipテーブル)を読み込んで文字列に返す
-        $iptable = split(" ", $ipdata);                     //IPデータ群を半角スペース区切りで分解して配列にいれる
+        $fingerhashdata = $client->getFileContents($path_fingerhashdata);   //FingerHashテーブルもDropboxから読み込み
+        $fingerhashtable = split(" ", $fingerhashdata);                     //FingerHashのデータ群も同じく分解し配列に格納
     }
-
-    //直近100アクセス以内に同じIPアドレスがあったらカウントしない
-    //……けど今のところはセッション判別法で複数カウント判別しようと思います。
-    //一応IPは記録したいので処理だけ残しとく
-    for($i=0;$i<100;$i++){
-        if($ip === $iptable[$i]){
+    
+    //IPログはずっと消さずにログをとっておきます
+    $ipdata = $client->getFileContents($path_ipdata);
+    $iptable = split(" ", $ipdata);
+    
+    //今日の分のIPログに同じIPアドレスがあったらカウントしない
+    //(実際はIPアドレスを判別処理に使わずログを取ってるだけです)
+    foreach ($iptable as &$value) {
+        if($value === $ip){
             $ipflag = false;
             break;
         }
     }
+    unset($value);
     
-    if($ipflag){          //セッションが存在しない場合カウントする
+    //今日の分のFingerHashログに同じハッシュがあったらカウントしない
+    foreach ($fingerhashtable as &$value) {
+        if($value === $fingerhash){
+            $repeatflag = false;
+            break;  //同じハッシュがあったのでハッシュテーブルチェックをbreakでやめる
+        }
+    }
+    unset($value);
     
-        $_SESSION['access'] = 1;
+    if($repeatflag){
         
         $counts[0]++;   //トータルカウント+1
         $counts[1]++;   //今日のカウント+1
-        array_unshift($iptable,$ip);        //アクセス元IPアドレスを配列の最初にプッシュする、この関数便利です！
+        
+        array_unshift($fingerhashtable,$fingerhash);        //FingerHash値を配列の最初にプッシュする
 
         $countdata = join(" ", $counts);    //counts配列に含まれる数値群を文字列に変換して格納する、区切り文字は半角スペース
-        $ipdata = join(" ",$iptable);       //これも同じく
+        $fingerhashdata = join(" ", $fingerhashtable);       //これも同じく
 
         $client->uploadFileContents($path_countdata,$countdata);    //文字列をファイルに書き込んでアップロード
-        $client->uploadFileContents($path_ipdata,$ipdata);          //これも同じく
+        $client->uploadFileContents($path_fingerhashdata,$fingerhashdata);          //これも同じく
     }
+    
     echo "<div class=\"counter-ty\">あくせすかうんた<br><br></div>\n";
     echo "<div class=\"counter-num\">",sprintf("%06d", $counts[0]),"</div>";
     echo "<div class=\"counter-ty\"><br>今日：", $counts[1], "　昨日：", $counts[2], "</div>\n";
-    echo $_REQUEST['fpk'];
-
+    
+    if($ipflag){
+        array_unshift($iptable,$ip);        //アクセス元IPアドレスを配列の最初にプッシュする
+        $ipdata = join(" ",$iptable);
+        $client->uploadFileContents($path_ipdata,$ipdata);
+    }
+    
 ?>
 
