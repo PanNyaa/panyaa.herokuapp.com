79f58bc4f8fb667902d7b79fadd6023a5c763fde
PanNyaa <pan.nyaa.ccc@gmail.com>
2016-09-26 10:50:19 +0900

    autoload.phpあれこれ

--- wp-content/themes/plain-blog/accesscounter.php
+++ wp-content/themes/plain-blog/accesscounter.php
@@ -40,7 +40,7 @@
 
     //手動で配置したdropbox-sdkを読み込む。ファイルパスはphpの場所に応じて適宜書き換えてください。
     //require_once dirname(__FILE__)."/../../../lib/Dropbox/autoload.php";
-    //require_once('/../../../../vendor/autoload.php'); //heroku上にdropbox-sdkがインストールできたのでどこからでもお手軽インクルード
+    require_once('/../../../vendor/autoload.php'); //heroku上にdropbox-sdkがインストールできたのでどこからでもお手軽インクルード
     //namespace Controllers;
 
     // \Dropbox を dbx としても記述できるように定義する
