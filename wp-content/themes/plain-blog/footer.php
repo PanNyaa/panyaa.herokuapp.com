ec9bd05867a539267c96b817c5c61e3a7bbd3a9b
PanNyaa <pan.nyaa.ccc@gmail.com>
2016-09-27 13:46:23 +0900

    headに記述してたCSS読み込みをいい感じにfooterに移動、あとhead-cleanerっていうプラグインの追加

--- wp-content/themes/plain-blog/footer.php
+++ wp-content/themes/plain-blog/footer.php
@@ -43,6 +43,8 @@
     
     </div> <!--main_container end-->
     <?php wp_footer(); ?>
+    <link rel="stylesheet" href="/dat/vs.css"/><!-- syntax highlight -->
+    <link rel="stylesheet" href="/dat/ex-highlight.css"/><!-- syntax highlightでWebフォントを使用するためのスタイル -->
     <script src="/dat/highlight.pack.js"></script>
     <script>hljs.initHighlighting()</script>
     <script src="/dat/ex-highlight.js"></script>
--- wp-content/themes/plain-blog/header.php
+++ wp-content/themes/plain-blog/header.php
@@ -17,8 +17,6 @@
 }
 ?>
     
-    <link rel="stylesheet" href="/dat/vs.css"/><!-- syntax highlight -->
-    <link rel="stylesheet" href="/dat/ex-highlight.css"/><!-- syntax highlightでWebフォントを使用するためのスタイル -->
     <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/faviconmayu.gif" />
     <meta charset="<?php bloginfo('charset'); ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
